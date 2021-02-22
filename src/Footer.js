d95ba60533e62d74683d15f67af6efbed01d812b
Brian Ross <brian.ross@students.makeschool.com>
2021-02-22 02:43:46 -0500

    added dynamic components and made new git repo

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className = "POPOSFooter">
+        <footer>
+  <p>Creator: Benny Cashman</p>
+  <p><a href="mailto:hege@example.com">cashmanbenny@gmail.com</a></p>
+</footer>
+
+        </div>
+    )
+
+
+}
+
+export default Footer
---
+++ src/POPOSSpace.js
@@ -0,0 +1,23 @@
+// src/Project.js
+
+import React from 'react'
+import './POPOSSpace.css'
+import logo from './logo.svg'
+
+function POPOSSpace(props) {
+  const { name, image, address } = props
+  return (
+    <div className="POPOSSpace">
+      <img 
+        src={`${process.env.PUBLIC_URL}images/${image}`} 
+        width="300" 
+        height="300" 
+        alt="Hello" 
+      />
+      <h1>{name}</h1>
+      <div>{address}</div>
+    </div>
+  )
+}
+
+export default POPOSSpace
