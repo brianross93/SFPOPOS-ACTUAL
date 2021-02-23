2e91272513a8ab4faf66bf1ff7043df0613ce95c
Brian Ross <brian.ross@students.makeschool.com>
2021-02-23 01:16:04 -0500

    finished SFPOPOS

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import Title from './Title';
 import POPOSList from './POPOSList';
+import POPOSDetails from './POPOSDetails';
+import { HashRouter as Router, Route } from 'react-router-dom'
+import About from './About'
 
 import Footer from './Footer';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Title />
-      <POPOSList />
+      <Route exact path="/" component={POPOSList}/>
       <Footer />
+      <Route exact path="/about" component={About}/>
+      <Route path="/details/:id" component={POPOSDetails} />
     </div>
+    </Router>
   );
 }
 
--- src/Title.js
+++ src/Title.js
@@ -1,14 +1,33 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 import './Title.css';
 
 function Title() {
-    return (
-      <div className="Title">
+  return (
+    <div className="Title">
+      <header>
         <h1>SFPOPOS</h1>
-              <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
-      </div>
-    )
-  }
+        <div className="Title-Subtitle">San Francisco Privately Owned Public Open Spaces</div>
+
+        <div>
+        <NavLink
+          className="nav-link"
+          activeClassName="nav-link-active"
+          exact
+          to="/">List
+          </NavLink>
+
+        <NavLink
+          className="nav-link"
+          activeClassName="nav-link-active"
+          to="/about">About
+          </NavLink>
+        </div>
+
+      </header>
+    </div>
+  )
+}
 
 export default Title
